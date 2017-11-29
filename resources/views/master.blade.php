d87f678637883b641ad4d6c350286eaa4a211afc
João Pedro Mantovani <jmantovani@unicamp.br>
2017-11-29 09:33:25 -0200

    Adiciona o Google Analytics em todas as páginas do jogo

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -11,6 +11,16 @@
 
 </head>
 <body>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-86211771-2"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+
+	  gtag('config', 'UA-86211771-2');
+	</script>
+
 
 	@section('navbar')
 		
